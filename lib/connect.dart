874f2d924c9e600b95e49bd6506aabdb8305d27d
sridhar-5 <kv.sridharsai@gmail.com>
2021-10-09 19:16:44 +0530

    some changes to main.dart

---
+++ lib/connect.dart
@@ -0,0 +1,1 @@
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:noteiteasy/record_page.dart';
 import 'package:firebase_core/firebase_core.dart'; //importing the firebase core plugin
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatefulWidget {
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  //fireflutter
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyAwesomeApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
+      },
+    );
+  }
+
+  MaterialApp MyAwesomeApp() {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Note It Easy',
@@ -20,4 +49,26 @@ class MyApp extends StatelessWidget {
       home: const RecordPage(),
     );
   }
+}
+
+MaterialApp SomethingWentWrong() {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Note It Easy',
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: const RecordPage(),
+  );
+}
+
+MaterialApp Loading() {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Note It Easy',
+    theme: ThemeData(
+      primarySwatch: Colors.red,
+    ),
+    home: const RecordPage(),
+  );
 }
