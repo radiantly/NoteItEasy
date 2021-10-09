db23a1710f3d07c57efa4a495c3aa127802b671f
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-10-09 17:03:13 +0530

    Fixed permissions

--- lib/record_button.dart
+++ lib/record_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
 class RecordButton extends StatefulWidget {
   const RecordButton({Key? key}) : super(key: key);
 
