8f9db677eda9c68b5cb7f4df98d296ecb84f400e
radiantly <buildingsomethingfun@gmail.com>
2021-10-09 18:03:34 +0530

    Generate memos in ListView

--- lib/record_page.dart
+++ lib/record_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:noteiteasy/globals.dart' as globals;
+import 'package:noteiteasy/memo_list.dart';
 import 'package:noteiteasy/record_button.dart';
 
 class RecordPage extends StatelessWidget {
@@ -16,17 +18,28 @@ class RecordPage extends StatelessWidget {
               width: MediaQuery.of(context).size.width,
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
+                    // TODO: Fix Gradient rotation here to be diagonal
                     colors: [globals.gradientPurple, globals.gradientPink]),
               )),
           SafeArea(
             child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+              padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Note it easy!",
-                      style: GoogleFonts.loveYaLikeASister(
-                          textStyle: const TextStyle(
-                              color: Colors.white, fontSize: 40))),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    child: Text("Note it easy!",
+                        style: GoogleFonts.loveYaLikeASister(
+                            textStyle: const TextStyle(
+                                color: Colors.white, fontSize: 40))),
+                  ),
+                  const Text("Your memos!",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold)),
+                  Expanded(child: MemoList())
                 ],
               ),
             ),
