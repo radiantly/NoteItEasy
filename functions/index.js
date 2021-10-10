1c63b55898a65a7718e3aa1ae0846f979039c56d
radiantly <buildingsomethingfun@gmail.com>
2021-10-10 18:50:04 +0530

    functions: Add basic transcription

--- functions/index.js
+++ functions/index.js
@@ -1,64 +1,55 @@
 const functions = require("firebase-functions");
-const { DISCORD_WEBHOOOK_URL, API_TOKEN } = require("./config.json");
+const {
+  DISCORD_WEBHOOK_URL,
+  ASSEMBLYAI_TRANSCRIPT_API,
+  ASSEMBLYAI_API_TOKEN,
+  WEBHOOK_CLOUD_FUNCTION_URL,
+} = require("./config.json");
 const axios = require("axios");
-const express = require("express");
-const app = express();
-
-const assemblyAi = axios.create({
-  baseURL: "https://api.assemblyai.com/v2",
-  headers: {
-    authorization: API_TOKEN,
-    "content-type": "application/json",
-  },
-});
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 exports.generateTranscript = functions.storage
   .object()
   .onFinalize(async (object) => {
     const objectPath = object.name.replace(/ /g, "%20").replace(/\//g, "%2F");
 
-    const downloadURL = `https://firebasestorage.googleapis.com/v0/b/${object.bucket}/o/${objectPath}?alt=media`;
+    const audio_url = `https://firebasestorage.googleapis.com/v0/b/${object.bucket}/o/${objectPath}?alt=media`;
 
-    await axios.post(DISCORD_WEBHOOOK_URL, {
-      content: `${object.name} has been uploaded in ${object.bucket}. Public download url is at ${downloadURL}`,
+    await axios.post(DISCORD_WEBHOOK_URL, {
+      content: `${object.name} has been uploaded in ${object.bucket}. Public download url is at ${audio_url}`,
     });
 
-    assembly
-      .get(`/transcript/`, {
-        audio_url: downloadURL,
-        webhook_url: "coming soon",
-      })
-      .then((res) => console.log(res.data))
-      .catch((err) => console.error(err));
+    await axios.post(
+      ASSEMBLYAI_TRANSCRIPT_API,
+      {
+        audio_url,
+        webhook_url: WEBHOOK_CLOUD_FUNCTION_URL,
+      },
+      {
+        headers: {
+          Authorization: ASSEMBLYAI_API_TOKEN,
+          "Content-Type": "application/json",
+        },
+      }
+    );
   });
 
-var transcript_status = "";
-var Transcript_id = "";
-
-//if we get a post request
-app.post("/", (request, response) => {
-  Transcript_id = request.body.transcript_id;
-  transcript_status = request.body.status;
-});
-
-const getUrl = `https://api.assemblyai.com/v2/transcript/${Transcript_id}`;
-
-// making a get request to the assemble ai api again to get the transcript
-var Transcript = "";
-assemblyAi.get(getUrl).then(async (response) => {
-  Transcript = response.body.text;
-
-  //debugging
-  await axios.post(DISCORD_WEBHOOOK_URL, {
-    content: `Transcript: ${Transcript}`,
-  });
-});
-
-exports.apiHackHarvard = functions.https.onRequest(app);
+exports.transcriptWebhook = functions.https.onRequest(
+  async (request, response) => {
+    const { status, transcript_id } = request.body;
+    console.log(status);
+    const transcript_response = await axios.get(
+      `${ASSEMBLYAI_TRANSCRIPT_API}/${transcript_id}`,
+      {
+        headers: {
+          Authorization: ASSEMBLYAI_API_TOKEN,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(transcript_response.data);
+    await axios.post(DISCORD_WEBHOOK_URL, {
+      content: `Transcription complete! ${transcript_response.data.text}`,
+    });
+    response.send();
+  }
+);
