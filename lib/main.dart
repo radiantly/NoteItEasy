524c4597378325d669fbb9bf694519484783a9b0
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-10-10 12:22:05 +0530

    Added firebase auth config

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:noteiteasy/record_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:noteiteasy/splash_page.dart'; //importing the firebase core plugin
 
-void main() {
+
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const App());
 }
 
